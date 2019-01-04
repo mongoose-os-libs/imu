d7e5069bc20bb50f6f7754c4501760bd89d77850
Pim van Pelt <pim@ipng.nl>
2019-01-04 21:32:51 +0100

    Complete ADXL345 driver

--- src/mgos_imu_adxl345.c
+++ src/mgos_imu_adxl345.c
@@ -29,7 +29,7 @@ bool mgos_imu_adxl345_detect(struct mgos_imu_acc *dev, void *imu_user_data) {
   if (device_id == MGOS_ADXL345_DEVID) {
     return true;
   }
-  LOG(LL_ERROR, ("Failed to detect MPU925x at I2C 0x%02x (device_id=0x%02x)", dev->i2caddr, device_id));
+  LOG(LL_ERROR, ("Failed to detect ADXL345 at I2C 0x%02x (device_id=0x%02x)", dev->i2caddr, device_id));
   return false;
 
   (void)imu_user_data;
@@ -40,11 +40,15 @@ bool mgos_imu_adxl345_create(struct mgos_imu_acc *dev, void *imu_user_data) {
     return false;
   }
 
-  mgos_i2c_write_reg_b(dev->i2c, dev->i2caddr, MGOS_ADXL345_REG_POWER_CTL, MGOS_ADXL345_POWER_MEAS);
-  mgos_i2c_write_reg_b(dev->i2c, dev->i2caddr, MGOS_ADXL345_REG_DATA_FORMAT, MGOS_ADXL345_FULL_RANGE | MGOS_ADXL345_RANGE_2G);
-  mgos_i2c_write_reg_b(dev->i2c, dev->i2caddr, MGOS_ADXL345_REG_BW_RATE, MGOS_ADXL345_RATE_100);
+  // POWER_CTL: 00; link=0; auto_sleep=0; measure=1; sleep=0; wakeup=00 (8Hz)
+  // DATA_FORMAT: SELF_TEST=0; SPI=0; INT_INVERT=0; 0; FULL_RES=1; Justify=0; Range=10 (8G)
+  // BW_RATE: 000; LOW_POWER=0; Rate=1010 (100Hz ODR, 50Hz filter)
+  mgos_i2c_write_reg_b(dev->i2c, dev->i2caddr, MGOS_ADXL345_REG_POWER_CTL, 0x08);
+  mgos_i2c_write_reg_b(dev->i2c, dev->i2caddr, MGOS_ADXL345_REG_DATA_FORMAT, 0x0A);
+  mgos_i2c_write_reg_b(dev->i2c, dev->i2caddr, MGOS_ADXL345_REG_BW_RATE, 0x0B);
 
-  dev->scale = 1. / 1024;
+  // FULL_RES: 3.9mg/LSB always
+  dev->scale = 0.0039 * G2MSS;
   return true;
 
   (void)imu_user_data;
@@ -62,7 +66,8 @@ bool mgos_imu_adxl345_read(struct mgos_imu_acc *dev, void *imu_user_data) {
   dev->ax = (data[0]) | (data[1] << 8);
   dev->ay = (data[2]) | (data[3] << 8);
   dev->az = (data[4]) | (data[5] << 8);
-  // LOG(LL_DEBUG, ("ax=%d ay=%d az=%d", dev->ax, dev->ay, dev->az));
+
+  LOG(LL_DEBUG, ("ax=%d ay=%d az=%d", dev->ax, dev->ay, dev->az));
   return true;
 
   (void)imu_user_data;
