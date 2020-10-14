af0d2960def428cf9dddc6104cb409c88bb97232
Sergio R. Caprile <scaprile@gmail.com>
2020-10-14 13:39:07 -0300

    Removed copyright banners, added warning message.
    Signed-off-by: Sergio R. Caprile <scaprile@gmail.com>

--- src/mgos_imu_mpu6886.c
+++ src/mgos_imu_mpu6886.c
@@ -1,31 +1,20 @@
-/*
- * Copyright 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include "mgos_imu_mpu6886.h"
-#include "mgos_imu_mpu60x0.h"
 #include "mgos.h"
 #include "mgos_i2c.h"
+#include "mgos_imu_mpu60x0.h"
 
 static bool mgos_imu_mpu6886_detect(struct mgos_i2c *i2c, uint8_t i2caddr) {
   if (!i2c) {
     return false;
   }
 
-  return MGOS_MPU6886_DEVID ==
-         mgos_i2c_read_reg_b(i2c, i2caddr, MGOS_MPU60X0_REG_WHO_AM_I);
+  if (MGOS_MPU6886_DEVID !=
+      mgos_i2c_read_reg_b(i2c, i2caddr, MGOS_MPU60X0_REG_WHO_AM_I)) {
+    return false;
+  }
+  LOG(LL_WARN, ("This driver is experimental and work in progress. Proceed "
+                "with caution!"));
+  return true;
 }
 
 bool mgos_imu_mpu6886_acc_detect(struct mgos_imu_acc *dev,
